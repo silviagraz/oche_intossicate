968eb90990fb8fe0317f275716f0fbea58d5a2ae
Luca Terrazzan <luca.terraz@gmail.com>
2020-04-17 22:51:57 +0200

    added oche database

--- prova_inferenza_oche.py
+++ prova_inferenza_oche.py
@@ -1,3 +1,19 @@
-oche_intossicate = [
-    { 'id': 1, 'glucosio': 279, 'intossicazione': False}
+database = [
+    { 'id': 1,  'glucosio': 279, 'intossicata': False },
+    { 'id': 2,  'glucosio': 340, 'intossicata': False },
+    { 'id': 3,  'glucosio': 325, 'intossicata': False },
+    { 'id': 4,  'glucosio': 227, 'intossicata': False },
+    { 'id': 5,  'glucosio': 277, 'intossicata': False },
+    { 'id': 6,  'glucosio': 337, 'intossicata': False },
+    { 'id': 7,  'glucosio': 250, 'intossicata': False },
+    { 'id': 8,  'glucosio': 290, 'intossicata': False },
+    { 'id': 9,  'glucosio': 293, 'intossicata': True  },
+    { 'id': 10, 'glucosio': 291, 'intossicata': True  },
+    { 'id': 11, 'glucosio': 289, 'intossicata': True  },
+    { 'id': 12, 'glucosio': 430, 'intossicata': True  },
+    { 'id': 13, 'glucosio': 510, 'intossicata': True  },
+    { 'id': 14, 'glucosio': 353, 'intossicata': True  },
+    { 'id': 15, 'glucosio': 318, 'intossicata': True  }
 ]
+
+print(database)
